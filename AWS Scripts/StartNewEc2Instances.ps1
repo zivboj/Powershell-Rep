3561b48e799f991b78a3fc870375bf43ce03774a
zivboj <zivboj@gmail.com>
2018-06-19 10:17:22 +1000

    Delete StartNewEc2Instances.ps1

--- AWS Scripts/StartNewEc2Instances.ps1
+++
@@ -1,4 +0,0 @@
-# Requires AWSPowershell.Core module to be installed
-# Requires Ec2Profile to be setup in powershell
-
-New-EC2Instance -ImageId 'amiID' -MaxCount 1 -MinCount 1 -InstanceType 'InstanceType eg t2.small' -KeyName 'PrivateKeyName' -SubnetId 'SubnetID' -SecurityGroupId 'SecGroupID' -Region 'Region' -verbose
